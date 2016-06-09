63ea2b1e9a6ee7ea654200a530a9699847bc42f3
Jeremy Cook <jeremy.cook.llb@live.com>
2016-06-09 21:39:35 +1200

    building the engine

---
+++ socialmediafeature/Login.php
@@ -0,0 +1,53 @@
+<?php
+require_once("MyFunctions.php");
+include_once "MYSQLDB.php";
+$host = 'localhost' ;
+$dbUser = 'root' ;
+$dbPass = '' ;
+$dbName = 'image_annotator' ;
+$db = new MySQL( $host, $dbUser , $dbPass , $dbName ) ;
+$db->selectDatabase();
+
+echo "Login";
+function isValidForm ( $theUserID  ) 
+{
+    $result = true;
+    if ( $theUserID == ""  )
+    {
+       $result = false;
+       print "Please enter a word or words for your search";
+    }
+return $result;
+}
+
+if($_SERVER['REQUEST_METHOD'] == "POST")
+{
+    // grab the variables from the form
+    $theUserID = $_POST["theUserID"];
+    //echo $theWords;
+    if ( isValidForm ( $theUserID )  )
+    {
+       // specify where to save the session variables
+        session_save_path("./");
+        session_start();
+      // register the session variables and load the next page
+        $_SESSION["theUserID"] = $theUserID ;
+        /*header ("Location:searchProductsA.php")*/ ;
+		$user = getAUser($db,$theUserID);
+		displayAUser($user);
+    }
+}  
+?>
+
+<form action="login.php" method="post">
+	<input type="text" name="theUserID">
+	<button type="submit" value="Search for user">Login User In</button>
+</form>
+<a href="registerNewUser.php">Register new user<a>
+
+<html>
+<body>
+<br><br>
+<a href="main.php">Return To Main Form</a>
+</body>
+</html>
