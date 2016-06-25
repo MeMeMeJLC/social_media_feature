6c9980b78c430e97e6cafe195423b02da4c1160a
Jeremy Cook <jeremy.cook.llb@live.com>
2016-06-25 23:14:04 +1200

    annotation icons aligned

--- socialmediafeature/annotate.php
+++ socialmediafeature/annotate.php
@@ -1,8 +1,8 @@
 <script>	
  
 function getAnAnnotationLocation(){
-		var x = event.clientX;
-		var y = event.clientY;
+		var x = event.clientX - 16.5;
+		var y = event.clientY - 17;
 		document.getElementById('annotationLocationX').value = x;
 		document.getElementById('annotationLocationY').value = y;	
 console.log(x);
